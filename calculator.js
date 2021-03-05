fed0f9b18c0d707f9b2d728822fcb9d304e6c556
Dominic Bruno <dominicbtuno@sbcglobal.net>
2021-03-04 19:34:46 -0600

    Updated File Structure

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (req, res) => {
+    console.log(`App running on server: ${port}`);
+});
