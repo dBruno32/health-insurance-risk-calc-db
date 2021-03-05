35c6d51ae8d29924797c6faef6baf63c1fd2edb8
Dominic Bruno <dominicbtuno@sbcglobal.net>
2021-03-04 19:50:42 -0600

    Updated File Structure

--- calculator.js
+++ calculator.js
@@ -1,7 +1,30 @@
+//  @author: Dominic Bruno
+//  @app: Health Insurance Risk Calculator
+//  @course: Software Engineering (Proff. Pogue), Spring 2021 | Lewis University
+//  @contact: dominicmbruno@lewisu.edu
+
 const express = require('express');
-const app = express();
+const path = require('path');
+const handlebars = require('express-handlebars');
 
 const port = process.env.PORT || 3000;
+
+const app = express();
+
+//  HBS (ext) - Handlebars View Engine
+//  @ Local DIR - 'view'
+app.engine('.hbs', handlebars({ 
+    defaultLayout: 'main', 
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+//  Static Files @ Local DIR - 'src'
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.use('/', require('./routes/index.js'));
+
+
 
 app.listen(port, (req, res) => {
     console.log(`App running on server: ${port}`);
