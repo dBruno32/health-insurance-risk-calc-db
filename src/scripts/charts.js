f2acb537f0eb714eb3764dee196facb0cc7793e2
Dominic Bruno <dominicbtuno@sbcglobal.net>
2021-03-06 19:19:06 -0600

    Updated Chart.js functionality w/ correct Data

--- src/scripts/charts.js
+++ src/scripts/charts.js
@@ -1,3 +1,9 @@
+const ageScore = document.getElementById('ageScore').innerHTML;
+const bmiScore = document.getElementById('bmiScore').innerHTML;
+const bpScore = document.getElementById('bpScore').innerHTML;
+const familyHistoryScore = document.getElementById('familyHistoryScore').innerHTML;
+
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     type: 'bar',
@@ -9,7 +15,7 @@ var chart = new Chart(ctx, {
             label: 'My First dataset',
             backgroundColor: 'rgb(255, 99, 132)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2]
+            data: [ageScore, bmiScore, bpScore, familyHistoryScore]
         }]
     },
 
