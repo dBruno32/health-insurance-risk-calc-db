6afcace2800c52e38e2c8df9e56cecb488f26bda
Dominic Bruno <dominicbtuno@sbcglobal.net>
2021-03-09 18:53:43 -0600

    Updated Routes / lost progress

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
 
 // Home 
 router.get('/', (req, res) => {
   res.render('index');
+});
+
+// Summary
+router.get('/summary', (req,res) => {
+  res.render('summary', {
+    layout: summary
+  });
+});
+
+// Collect & calculate results from form
+router.post('/calc', (req, res) => {
+  res.redirect('/summary');
 });
 
 module.exports = router;
